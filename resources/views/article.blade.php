fff4320d7892a71480a10789e0554fcc04b220d9
Ndeffoloic <96016785+Ndeffoloic@users.noreply.github.com>
2023-06-18 20:25:22 +0200

    Update article.blade.php
    Dans cette nouvelle ligne 17, nous utilisons la fonction nl2br() pour convertir les sauts de ligne en balises <br>, et la fonction e() pour échapper les caractères spéciaux HTML dans le contenu. Les accolades doubles {!! !!} sont utilisées pour empêcher l'échappement HTML du contenu lors de l'affichage.

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -14,7 +14,7 @@
   <div class="bg-white rounded-lg w-3/4 container px-5 py-16 mx-auto flex flex-col items-center">
     <h1 class="m-8 text-center sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">{{$article->titre}}</h1>
     <img class="m-8 rounded-lg w-3/4" src="{{asset($article->lienimage)}}" alt="" class="mx-auto">
-    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">{{$article->contenu.str_replace("\n","<br>",$article->contenu)}}</p>
+    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">{!! nl2br(e($article->contenu)) !!}</p>
   </div>
 </body>
 <footer>
